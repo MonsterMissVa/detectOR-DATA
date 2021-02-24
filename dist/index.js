dea1a6a4e60f790e739faa41ce58928dfbdcebaf
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-02-24 15:29:33 +0000

    some new tests

--- dist/index.js
+++ dist/index.js
@@ -90,4 +90,29 @@ function escapeData(s) {
 function escapeProperty(s) {
     return utils_1.toCommandValue(s)
         .replace(/%/g, '%25')
-        
+        .replace(/\r/g, '%0D')
+        .replace(/\n/g, '%0A')
+        .replace(/:/g, '%3A')
+        .replace(/,/g, '%2C');
+}
+//# sourceMappingURL=command.js.map
+
+/***/ }),
+
+/***/ 2186:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
+
+
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = 

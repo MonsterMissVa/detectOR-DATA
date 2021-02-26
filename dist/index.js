2afdb96350fef73bb87f96d38443cb32947b620a
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-02-26 08:20:59 +0000

    fixed testdata

--- dist/index.js
+++ dist/index.js
@@ -157,4 +157,24 @@ var ExitCode;
 // Variables
 //-----------------------------------------------------------------------
 /**
- * Sets env variable for this action and future actions 
+ * Sets env variable for this action and future actions in the job
+ * @param name the name of the variable to set
+ * @param val the value of the variable. Non-string values will be converted to a string via JSON.stringify
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function exportVariable(name, val) {
+    const convertedVal = utils_1.toCommandValue(val);
+    process.env[name] = convertedVal;
+    const filePath = process.env['GITHUB_ENV'] || '';
+    if (filePath) {
+        return file_command_1.issueFileCommand('ENV', file_command_1.prepareKeyValueMessage(name, val));
+    }
+    command_1.issueCommand('set-env', { name }, convertedVal);
+}
+exports.exportVariable = exportVariable;
+/**
+ * Registers a secret which will get masked from logs
+ * @param secret value of the secret
+ */
+function setSecret(secret) {
+    command_1.issueComman

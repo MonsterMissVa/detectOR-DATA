b5a6147536ee5df553284a4513b5379ff93a8d35
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-04-12 08:25:58 +0000

    log to file

--- dist/index.js
+++ dist/index.js
@@ -3581,4 +3581,22 @@ function extractUrlVariableNames(url) {
 }
 
 function omit(object, keysToOmit) {
-  retu
+  return Object.keys(object).filter(option => !keysToOmit.includes(option)).reduce((obj, key) => {
+    obj[key] = object[key];
+    return obj;
+  }, {});
+}
+
+// Based on https://github.com/bramstein/url-template, licensed under BSD
+// TODO: create separate package.
+//
+// Copyright (c) 2012-2014, Bram Stein
+// All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the dis

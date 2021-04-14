b51e8baa92f7e4e83dd2c1465a03c60cd74d2bcf
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-04-14 21:44:56 +0000

    MIPS: Loongson: Make platform serial setup always built-in.

--- dist/index.js
+++ dist/index.js
@@ -3801,4 +3801,35 @@ function parse(options) {
         body = remainingParameters;
       } else {
         headers["content-length"] = 0;
- 
+      }
+    }
+  } // default content-type for JSON if body is set
+
+
+  if (!headers["content-type"] && typeof body !== "undefined") {
+    headers["content-type"] = "application/json; charset=utf-8";
+  } // GitHub expects 'content-length: 0' header for PUT/PATCH requests without body.
+  // fetch does not allow to set `content-length` header, but we can set body to an empty string
+
+
+  if (["PATCH", "PUT"].includes(method) && typeof body === "undefined") {
+    body = "";
+  } // Only return body/request keys if present
+
+
+  return Object.assign({
+    method,
+    url,
+    headers
+  }, typeof body !== "undefined" ? {
+    body
+  } : null, options.request ? {
+    request: options.request
+  } : null);
+}
+
+function endpointWithDefaults(defaults, route, options) {
+  return parse(merge(defaults, route, options));
+}
+
+function withDefaults(oldDefa

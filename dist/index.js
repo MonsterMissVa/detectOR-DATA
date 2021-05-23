f1335a9f41bb8fecd56264264cc842a3e527eec6
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-05-23 17:28:33 +0000

    remove blank

--- dist/index.js
+++ dist/index.js
@@ -5262,4 +5262,27 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 var Bottleneck = _interopDefault(__nccwpck_require__(1174));
 
 // @ts-ignore
-async function errorReque
+async function errorRequest(octokit, state, error, options) {
+  if (!error.request || !error.request.request) {
+    // address https://github.com/octokit/plugin-retry.js/issues/8
+    throw error;
+  } // retry all >= 400 && not doNotRetry
+
+
+  if (error.status >= 400 && !state.doNotRetry.includes(error.status)) {
+    const retries = options.request.retries != null ? options.request.retries : state.retries;
+    const retryAfter = Math.pow((options.request.retryCount || 0) + 1, 2);
+    throw octokit.retry.retryRequest(error, retries, retryAfter);
+  } // Maybe eventually there will be more cases here
+
+
+  throw error;
+}
+
+// @ts-ignore
+
+async function wrapRequest(state, request, options) {
+  const limiter = new Bottleneck(); // @ts-ignore
+
+  limiter.on("failed", function (error, info) {
+    const maxRetries = 

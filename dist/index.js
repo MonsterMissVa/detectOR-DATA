433020cb1273704c66934e697009b818609654fa
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-06-01 16:20:16 +0000

    Add about

--- dist/index.js
+++ dist/index.js
@@ -6025,4 +6025,43 @@ function addHook(state, kind, name, hook) {
 
 module.exports = register;
 
-function register(state, name
+function register(state, name, method, options) {
+  if (typeof method !== "function") {
+    throw new Error("method for before hook must be a function");
+  }
+
+  if (!options) {
+    options = {};
+  }
+
+  if (Array.isArray(name)) {
+    return name.reverse().reduce(function (callback, name) {
+      return register.bind(null, state, name, callback, options);
+    }, method)();
+  }
+
+  return Promise.resolve().then(function () {
+    if (!state.registry[name]) {
+      return method(options);
+    }
+
+    return state.registry[name].reduce(function (method, registered) {
+      return registered.hook.bind(null, method, options);
+    }, method)();
+  });
+}
+
+
+/***/ }),
+
+/***/ 6819:
+/***/ ((module) => {
+
+module.exports = removeHook;
+
+function removeHook(state, name, method) {
+  if (!state.registry[name]) {
+    return;
+  }
+
+  var index

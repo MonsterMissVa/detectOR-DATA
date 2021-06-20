95477c9d61915b7de67ae7d60ebff2dc5b1183ba
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-06-20 15:16:18 +0000

    done for the night

--- dist/index.js
+++ dist/index.js
@@ -7644,4 +7644,43 @@ module.exports = realpath
 realpath.realpath = realpath
 realpath.sync = realpathSync
 realpath.realpathSync = realpathSync
-realpath.monkeypatch = monkeypa
+realpath.monkeypatch = monkeypatch
+realpath.unmonkeypatch = unmonkeypatch
+
+var fs = __nccwpck_require__(7147)
+var origRealpath = fs.realpath
+var origRealpathSync = fs.realpathSync
+
+var version = process.version
+var ok = /^v[0-5]\./.test(version)
+var old = __nccwpck_require__(1734)
+
+function newError (er) {
+  return er && er.syscall === 'realpath' && (
+    er.code === 'ELOOP' ||
+    er.code === 'ENOMEM' ||
+    er.code === 'ENAMETOOLONG'
+  )
+}
+
+function realpath (p, cache, cb) {
+  if (ok) {
+    return origRealpath(p, cache, cb)
+  }
+
+  if (typeof cache === 'function') {
+    cb = cache
+    cache = null
+  }
+  origRealpath(p, cache, function (er, result) {
+    if (newError(er)) {
+      old.realpath(p, cache, cb)
+    } else {
+      cb(er, result)
+    }
+  })
+}
+
+function realpathSync (p, cache) {
+  if (ok) {
+    

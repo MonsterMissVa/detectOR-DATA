65951807af625d05c73f60075040f92210802d5f
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-06-29 06:09:02 +0000

    setup blog directory

--- dist/index.js
+++ dist/index.js
@@ -7946,4 +7946,35 @@ exports.realpath = function realpath(p, cache, cb) {
   // walk down the path, swapping out linked pathparts for their real
   // values
   function LOOP() {
-    // stop if scanned 
+    // stop if scanned past end of path
+    if (pos >= p.length) {
+      if (cache) cache[original] = p;
+      return cb(null, p);
+    }
+
+    // find the next part
+    nextPartRe.lastIndex = pos;
+    var result = nextPartRe.exec(p);
+    previous = current;
+    current += result[0];
+    base = previous + result[1];
+    pos = nextPartRe.lastIndex;
+
+    // continue if not a symlink
+    if (knownHard[base] || (cache && cache[base] === base)) {
+      return process.nextTick(LOOP);
+    }
+
+    if (cache && Object.prototype.hasOwnProperty.call(cache, base)) {
+      // known symbolic link.  no need to stat again.
+      return gotResolvedLink(cache[base]);
+    }
+
+    return fs.lstat(base, gotStat);
+  }
+
+  function gotStat(err, stat) {
+    if (err) return cb(err);
+
+    // if not a symlink, skip to the next path part
+    if (!stat.isSymbolicLink(

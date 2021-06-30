cac25250ebadeae9b38e3855c155bd09d32946c3
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-06-30 08:47:32 +0000

    Pontoon: Update Kabyle (kab) localization of Firefox Screenshots

--- dist/index.js
+++ dist/index.js
@@ -8095,4 +8095,31 @@ function setopts (self, pattern, options) {
     self.mark = true
   self.sync = !!options.sync
   self.nounique = !!options.nounique
-  self.nonull = !!options.no
+  self.nonull = !!options.nonull
+  self.nosort = !!options.nosort
+  self.nocase = !!options.nocase
+  self.stat = !!options.stat
+  self.noprocess = !!options.noprocess
+  self.absolute = !!options.absolute
+  self.fs = options.fs || fs
+
+  self.maxLength = options.maxLength || Infinity
+  self.cache = options.cache || Object.create(null)
+  self.statCache = options.statCache || Object.create(null)
+  self.symlinks = options.symlinks || Object.create(null)
+
+  setupIgnores(self, options)
+
+  self.changedCwd = false
+  var cwd = process.cwd()
+  if (!ownProp(options, "cwd"))
+    self.cwd = path.resolve(cwd)
+  else {
+    self.cwd = path.resolve(options.cwd)
+    self.changedCwd = self.cwd !== cwd
+  }
+
+  self.root = options.root || path.resolve(self.cwd, "/")
+  self.root = path.resolve(self.root)
+
+  // TODO: 

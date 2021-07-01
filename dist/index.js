8ab0553bcf4faf5dd32cec1df0ac229f7983baba
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-07-01 18:13:11 +0000

    Bump version to 4.0.2

--- dist/index.js
+++ dist/index.js
@@ -8282,3 +8282,24 @@ function childrenIgnored (self, path) {
 //   If there is no more remaining, then stat(PREFIX) and
 //   add to matches if it succeeds.  END.
 //
+// If inGlobStar and PREFIX is symlink and points to dir
+//   set ENTRIES = []
+// else readdir(PREFIX) as ENTRIES
+//   If fail, END
+//
+// with ENTRIES
+//   If pattern[n] is GLOBSTAR
+//     // handle the case where the globstar match is empty
+//     // by pruning it out, and testing the resulting pattern
+//     PROCESS(pattern[0..n] + pattern[n+1 .. $], false)
+//     // handle other cases.
+//     for ENTRY in ENTRIES (not dotfiles)
+//       // attach globstar + tail onto the entry
+//       // Mark that this entry is a globstar match
+//       PROCESS(pattern[0..n] + ENTRY + pattern[n .. $], true)
+//
+//   else // not globstar
+//     for ENTRY in ENTRIES (not dotfiles, unless pattern[n] is dot)
+//       Test ENTRY against pattern[n]
+//       If fails, continue
+//       If passes, PROCESS(pattern[0..n] + item

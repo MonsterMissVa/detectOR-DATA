e2a040510c8292642b470a00045d76b874021e25
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-07-10 22:26:10 +0000

    add uuid

--- dist/index.js
+++ dist/index.js
@@ -9119,4 +9119,29 @@ function parseCommaParts(str) {
   var p = pre.split(',');
 
   p[p.length-1] += '{' + body + '}';
-  var 
+  var postParts = parseCommaParts(post);
+  if (post.length) {
+    p[p.length-1] += postParts.shift();
+    p.push.apply(p, postParts);
+  }
+
+  parts.push.apply(parts, p);
+
+  return parts;
+}
+
+function expandTop(str) {
+  if (!str)
+    return [];
+
+  // I don't know why Bash 4.3 does this, but it does.
+  // Anything starting with {} will have the first two bytes preserved
+  // but *only* at the top level, so {},a}b will not expand to anything,
+  // but a{},b}c will be expanded to [a}c,abc].
+  // One could argue that this is a bug in Bash, but since the goal of
+  // this module is to match Bash's rules, we escape a leading {}
+  if (str.substr(0, 2) === '{}') {
+    str = '\\{\\}' + str.substr(2);
+  }
+
+  return expand(escapeBraces(st

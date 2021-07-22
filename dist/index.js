feb3bd45cf4e5f45b31aca04c52d2f7fc84502ed
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-07-22 10:02:45 +0000

    :muscle: bump ??? distribution dist/snuggsi.min.es.js

--- dist/index.js
+++ dist/index.js
@@ -10227,4 +10227,30 @@ function globSync (pattern, options) {
   return new GlobSync(pattern, options).found
 }
 
-fu
+function GlobSync (pattern, options) {
+  if (!pattern)
+    throw new Error('must provide pattern')
+
+  if (typeof options === 'function' || arguments.length === 3)
+    throw new TypeError('callback provided to sync glob\n'+
+                        'See: https://github.com/isaacs/node-glob/issues/167')
+
+  if (!(this instanceof GlobSync))
+    return new GlobSync(pattern, options)
+
+  setopts(this, pattern, options)
+
+  if (this.noprocess)
+    return this
+
+  var n = this.minimatch.set.length
+  this.matches = new Array(n)
+  for (var i = 0; i < n; i ++) {
+    this._process(this.minimatch.set[i], i, false)
+  }
+  this._finish()
+}
+
+GlobSync.prototype._finish = function () {
+  assert.ok(this instanceof GlobSync)
+  if (this.realpath) {

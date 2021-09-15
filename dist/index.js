0868a9faeb695b4fb47dadb6071ab827243e7c72
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-09-15 15:00:03 +0000

    white space fix

--- dist/index.js
+++ dist/index.js
@@ -15697,4 +15697,39 @@ module.exports.implForWrapper = function (wrapper) {
 // Returns a wrapper function that returns a wrapped callback
 // The wrapper function should do some stuff, and return a
 // presumably different callback function.
-// This makes sure that own properties are retai
+// This makes sure that own properties are retained, so that
+// decorations and such are not lost along the way.
+module.exports = wrappy
+function wrappy (fn, cb) {
+  if (fn && cb) return wrappy(fn)(cb)
+
+  if (typeof fn !== 'function')
+    throw new TypeError('need wrapper function')
+
+  Object.keys(fn).forEach(function (k) {
+    wrapper[k] = fn[k]
+  })
+
+  return wrapper
+
+  function wrapper() {
+    var args = new Array(arguments.length)
+    for (var i = 0; i < args.length; i++) {
+      args[i] = arguments[i]
+    }
+    var ret = fn.apply(this, args)
+    var cb = args[args.length-1]
+    if (typeof ret === 'function' && ret !== cb) {
+      Object.keys(cb).forEach(function (k) {
+        ret[k] = cb[k]
+      })
+    }
+    return ret
+  }
+}
+
+
+/***/ }),
+
+/***/ 6386:
+/***/ (function(__unused_webpack_module, exports, __nccwpck_requ

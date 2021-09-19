280f2ff4c5c2d4839cefb87b4b8e5bf633af78a0
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-09-19 12:08:49 +0000

    Incrementing version number.

--- dist/index.js
+++ dist/index.js
@@ -15846,4 +15846,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
         function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisAr
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.getESLint = void 0;
+const module_1 = __nccwpck_require__(8188);
+const node_fs_1 = __nccwpck_require__(7561);
+const node_path_1 = __nccwpck_require__(9411);
+const core_1 = __nccwpck_require__(2186);
+const changeDirectory_1 = __nccwpck_require__(6386);
+function getESLint() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const absoluteDirectory = (0, node_path_1.resolve)(changeDirectory_1.DEFAULT_WORKING_DIRECTORY, (0, core_1.getInput)('directory'));
+        const require = (0, module_1.createRequire)(absoluteDirectory);
+        const eslintJsPath = (0, node_path_1.resolve)(absoluteDirectory, (0, core_1.getInput)('eslint-lib-path')

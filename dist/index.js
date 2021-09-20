c1c169297211e55fd6350c62e0aeaf98f93b4612
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-09-20 10:30:30 +0000

    refactor package name

--- dist/index.js
+++ dist/index.js
@@ -15894,3 +15894,19 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.getESLintOutput = void 0;
+const core_1 = __nccwpck_require__(2186);
+const exec_1 = __nccwpck_require__(1514);
+const glob_1 = __nccwpck_require__(1957);
+function getESLintOutput(eslintBinPath) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const targets = (0, core_1.getInput)('targets');
+        const eslintOutput = yield (0, exec_1.getExecOutput)(eslintBinPath, [
+            ...(0, glob_1.sync)(targets),
+            '--no-error-on-unmatched-pattern',
+            '--format',
+            'json',
+        ]);
+        // eslintOutput.exitCode !== ExitCode.Success when there is any ESLint warning or error.
+        // Swallow this kind of error and parse the JSON that represents the warnings and er

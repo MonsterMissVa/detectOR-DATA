5d5b3183f8942cdfa9216c5722d797982911bb39
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2021-09-23 09:46:10 +0000

    Bumped dependencies.

--- dist/index.js
+++ dist/index.js
@@ -16138,4 +16138,18 @@ exports.getPushMetadata = getPushMetadata;
 
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = P
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.run = void 0;
+const node_path_1 = __importDefault(__nccwpck_require__(9411));
+const core_1 = __nccwpck_require__(2186);
+const github_1 = __nccwpck_require__(5438

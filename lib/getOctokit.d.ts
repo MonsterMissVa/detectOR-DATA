10c43ad1c4a1e0974d4dc6232e9b28891a3e40bb
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2022-10-21 14:28:32 +0000

    Added integration tests.

---
+++ lib/getOctokit.d.ts
@@ -0,0 +1,5 @@
+export declare function getOctokit(): import("@octokit/core").Octokit & import("@octokit/plugin-rest-endpoint-methods/dist-types/types").Api & {
+    paginate: import("@octokit/plugin-paginate-rest").PaginateInterface;
+} & {
+    retry: {
+        retryRequest: (error: import("@octokit/request-error").RequestError, retries: 

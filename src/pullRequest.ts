dd53bcd10b72431d9d59cd8294ba36192c9a07e3
MonsterMissVa <104583074+MonsterMissVa@users.noreply.github.com>
2022-11-18 14:18:04 +0000

    md/raid10: close race that lose writes lost when replacement completes.

--- src/pullRequest.ts
+++ src/pullRequest.ts
@@ -12,4 +12,39 @@ import {
   startGroup,
 } from '@actions/core';
 import { getIndexedModifiedLines } from './getIndexedModifiedLines';
-import { getOctokit } fr
+import { getOctokit } from './getOctokit';
+
+type ReviewSuggestion = {
+  start_side?: 'RIGHT';
+  start_line?: number;
+  side: 'RIGHT';
+  line: number;
+  body: string;
+};
+
+type ReviewComment = ReviewSuggestion & { path: string };
+
+const REVIEW_BODY = "ESLint doesn't pass. Please fix all ESLint issues.";
+
+export async function getPullRequestFiles(
+  owner: string,
+  repo: string,
+  pullRequestNumber: number,
+  octokit: Octokit & Api,
+) {
+  const response = await octokit.rest.pulls.listFiles({
+    owner,
+    repo,
+    pull_number: pullRequestNumber,
+  });
+  info(`Files: (${response.data.length})`);
+  return response.data;
+}
+
+export async function getReviewComments(
+  owner: string,
+  repo: string,
+  pullRequestNumber: number,
+  octokit: Octokit & Api,
+) {
+  const reviews = await octokit.rest.pulls.listReviews({
